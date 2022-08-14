1a4d187087f3ca741907acbe46dcacbe9b2bf4a4
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-08-13 23:52:03 -0400

    Complete Part1 step7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,11 +13,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  // const [average, setAverage] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
-  
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  const sum = () => good + neutral + bad
+  const average = () => {
+    let average = (good * 1 + neutral * 0 + bad * -1) / sum()
+    if (isNaN(average)) average = 0
+    return average
+  }
+  const positive = () => {
+    let positive = (good / sum()) * 100
+    if (isNaN(positive)) positive = 0
+    return positive
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -29,6 +42,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {sum()}</div>
+      <div>average {average()}</div>
+      <div>positive {positive()} %</div>
     </div>
   )
 }
