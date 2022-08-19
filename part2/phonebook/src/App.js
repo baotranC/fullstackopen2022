cf766d76580f4c1772bc09d3901572210f309f9a
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-08-19 16:45:44 -0400

    Complete 2.10: The Phonebook Step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,43 @@
 import { useState } from 'react'
 
-const Person = ({ person }) => {
+const Filter = ({ filter, handleFilterChange }) => {
+  return <div>filter shown with <input value={filter} onChange={handleFilterChange} /></div>
+}
+
+const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
   return (
-    <li>{person.name} {person.number}</li>
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({personsToShow}) => {
+  return (
+    <ul>
+      {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+    </ul>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-123456'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     // console.log(event.target.value)
@@ -26,9 +48,9 @@ const App = () => {
     event.preventDefault()
     if (persons.find(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`)
-    } else if (persons.find(person => person.number.match("^[0-9]-*") == null)){
+    } else if (persons.find(person => person.number.match("^[0-9]-*") == null)) {
       alert(`${newNumber} is not a valid number`)
-    } 
+    }
     else {
       const nameObject = {
         name: newName,
@@ -55,24 +77,14 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>filter shown with <input value={filter} onChange={handleFilterChange}/></div>
+      <Filter filter={filter} handleFilterChange={handleFilterChange}></Filter>
 
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person => <Person key={person.name} person={person}></Person>)}
-      </ul>
+      <h3>Add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange}
+        newNumber={newNumber} handleNumberChange={handleNumberChange}></PersonForm>
+
+      <h3>Numbers</h3>
+      <Persons personsToShow={personsToShow}></Persons>
     </div>
   )
 }
