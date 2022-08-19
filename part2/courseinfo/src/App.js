6b71150de93374b72ba81909e50004f45579b811
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-08-18 21:56:55 -0400

    Complete 2.1: Course information step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,69 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+        {props.part} {props.exercise}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  const parts = props.course.parts.map((p, index) => <Part key={index} part={p.name} exercise={p.exercises}/>
+  )
+  
+  return (
+    <div>
+      {parts}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.course.parts.reduce((accumulator, current) => accumulator + current.exercises, 0)
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Course = ({course}) => {
+    return (
+    <div>
+      <Header course={course} />
+      <Content course={course}/>
+      <Total course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
