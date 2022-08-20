97c12514fa4ca3498e40635c9db9b3874270f858
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-08-20 01:11:46 -0400

    Complete 2.14: Data for countries, step3

---
+++ part2/countries/README.md
@@ -0,0 +1,9 @@
+
+
+
+
+TODO: Need to configure environment variable using .env file
+
+# .env
+
+REACT_APP_API_KEY=[Enter API key]
--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,26 +2,47 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const DisplayCountryDetail = ({ country }) => {
+  const api_key = process.env.REACT_APP_API_KEY
+  const [weather, setWeather] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}&units=metric`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
+
   return (
     <div>
       <h1>{country.name.common}</h1>
       <div>capital {country.capital}</div>
       <div>area {country.area}</div>
 
-      <h2>languages</h2>
+      <h3>languages</h3>
       <ul>
         {Object.keys(country.languages).map(key => {
           return <li key={key}>{country.languages[key]}</li>
         })}
       </ul>
       <img src={country.flags.png} alt="Flag"></img>
+
+      {weather.main ? (
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <div>temperature {weather.main.temp} Celcius</div>
+          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="Weather icon"></img>
+          <div>wind {weather.wind.speed} m/s</div>
+        </div>
+
+      ) : null}
     </div>
   )
 }
 
 const DisplayCountry = ({ country }) => {
   const [showDetail, setShowDetail] = useState(false)
-  
+
   return (
     <div>
       <div>{country.name.common} <button onClick={() => setShowDetail(!showDetail)}>show</button></div>
@@ -41,7 +62,7 @@ const DisplayCountries = ({ countries }) => {
     return (
       <div>
         {countries.map(country => {
-          return(<DisplayCountry key={country.name.common} country={country}></DisplayCountry>)
+          return (<DisplayCountry key={country.name.common} country={country}></DisplayCountry>)
         })}
       </div>
     )
