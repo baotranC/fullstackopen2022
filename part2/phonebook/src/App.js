fae24a54f91b5631a66b45fd48177fc87517a897
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-08-20 08:36:08 -0400

    Refactor components

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,33 +1,8 @@
 import { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 import personService from './services/persons'
-
-const Filter = ({ filter, handleFilterChange }) => {
-  return <div>filter shown with <input value={filter} onChange={handleFilterChange} /></div>
-}
-
-const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
-  return (
-    <form onSubmit={addPerson}>
-      <div>
-        name: <input value={newName} onChange={handleNameChange} />
-      </div>
-      <div>
-        number: <input value={newNumber} onChange={handleNumberChange} />
-      </div>
-      <div>
-        <button type="submit">add</button>
-      </div>
-    </form>
-  )
-}
-
-const Persons = ({ personsToShow }) => {
-  return (
-    <ul>
-      {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
-    </ul>
-  )
-}
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -71,12 +46,10 @@ const App = () => {
   }
 
   const handleNumberChange = (event) => {
-    // console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
   const handleFilterChange = (event) => {
-    // console.log(event.target.value)
     setFilter(event.target.value)
   }
 
