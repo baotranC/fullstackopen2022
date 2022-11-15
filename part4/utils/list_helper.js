4a3e82e365e7945b1d57bca8d3b56858ebf001a1
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-11-14 19:02:19 -0500

    Complete 4.4 and 4.5, tests

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,25 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	const totals = blogs.map(blog => blog.likes).reduce((prev, current) => prev + current);
+	return totals;
+}
+
+const favoriteBlog = (blogs) => {
+	const favoriteBlog = blogs.reduce((prev, current) => prev.likes > current.likes ? prev : current);
+
+	const favoriteBlogFormatted = {
+		title: favoriteBlog.title,
+		author: favoriteBlog.author,
+		likes: favoriteBlog.likes
+	}
+
+	return favoriteBlogFormatted;
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes,
+	favoriteBlog
 }
