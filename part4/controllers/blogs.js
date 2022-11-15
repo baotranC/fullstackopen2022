40e75764d967f7801c17af0ecc11a2e8c94cb1c4
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-11-15 18:58:18 -0500

    Refactor to async await instead of promise

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,44 +2,41 @@ const notesRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 /// APIs: HTTP METHODS
-notesRouter.get('/', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
+notesRouter.get('/', async (request, response) => {
+	const blogs = await Blog.find({})
+	response.json(blogs)
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
 	// validation
 	const body = request.body
-	if (!body.title) {
-		return response.status(400).json({
-			error: 'title missing'
-		})
-	} else if (!body.author) {
-		return response.status(400).json({
-			error: 'author missing'
-		})
-	} else if (!body.url) {
-		return response.status(400).json({
-			error: 'url missing'
-		})
-	}
-	else if (!body.likes) {
-		return response.status(400).json({
-			error: 'likes missing'
-		})
-	}
+	// if (!body.title) {
+	// 	return response.status(400).json({
+	// 		error: 'title missing'
+	// 	})
+	// } else if (!body.author) {
+	// 	return response.status(400).json({
+	// 		error: 'author missing'
+	// 	})
+	// } else if (!body.url) {
+	// 	return response.status(400).json({
+	// 		error: 'url missing'
+	// 	})
+	// }
+	// else if (!body.likes) {
+	// 	return response.status(400).json({
+	// 		error: 'likes missing'
+	// 	})
+	// }
 
 	const blog = new Blog(body)
 
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-		.catch(error => next(error))
+	try {
+		const saveBlog = await blog.save()
+		response.status(201).json(saveBlog)
+	} catch (exception) {
+		next(exception)
+	}
 })
 
 module.exports = notesRouter
