ce6bc31930bf3e124d6e201bdadacfa5a50427c5
bao <bao-tran.cong-huyen-ton-nu.1@etsmtl.net>
2022-11-14 19:44:06 -0500

    Fix number exercices, Complete 4.6

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -13,6 +13,7 @@ test('dummy returns one', () => {
 	expect(result).toBe(1)
 })
 
+// 4.4
 describe('total likes', () => {
 	const listWithOneBlog = [
 		{
@@ -66,7 +67,7 @@ describe('total likes', () => {
 	})
 })
 
-// 4.4
+// 4.5
 describe('blog with the most likes', () => {
 	const expectedResult = {
 		title: "Canonical string reduction",
@@ -108,6 +109,62 @@ describe('blog with the most likes', () => {
 })
 
 // 4.6
+describe('author with the most likes', () => {
+	const expectedResult = {
+		author: "Edsger W. Dijkstra",
+		blogs: 2
+	}
+
+	const blogs = [
+		{
+			_id: "5a422a851b54a676234d17f7",
+			title: "React patterns",
+			author: "Michael Chan",
+			url: "https://reactpatterns.com/",
+			likes: 7,
+			__v: 0
+		},
+		{
+			_id: "5a422aa71b54a676234d17f8",
+			title: "Go To Statement Considered Harmful",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+			likes: 5,
+			__v: 0
+		},
+		{
+			_id: "5a422b3a1b54a676234d17f9",
+			title: "Canonical string reduction",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+			likes: 12,
+			__v: 0
+		},
+		{
+			_id: "5a422a851b54a676234d17f6",
+			title: "React patterns",
+			author: "Michael Chan",
+			url: "https://reactpatterns.com/",
+			likes: 10,
+			__v: 0
+		},
+		{
+			_id: "5a422a851b54a676334d17f6",
+			title: "World",
+			author: "Hello",
+			url: "url hello",
+			likes: 8,
+			__v: 0
+		},
+	]
+
+	test('when list has multiple blogs, return author with the most likes and total number of likes', () => {
+		const result = listHelper.mostBlogs(blogs)
+		expect(result).toEqual(expectedResult)
+	})
+})
+
+// 4.7
 describe('author with the most likes', () => {
 	const expectedResult = {
 		author: "Edsger W. Dijkstra",
@@ -162,4 +219,3 @@ describe('author with the most likes', () => {
 		expect(result).toEqual(expectedResult)
 	})
 })
-
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,12 +1,15 @@
+// 4.3
 const dummy = (blogs) => {
 	return 1
 }
 
+// 4.4
 const totalLikes = (blogs) => {
 	const totals = blogs.map(blog => blog.likes).reduce((prev, current) => prev + current);
 	return totals;
 }
 
+// 4.5
 const favoriteBlog = (blogs) => {
 	const favoriteBlog = blogs.reduce((prev, current) => prev.likes > current.likes ? prev : current);
 
@@ -19,6 +22,30 @@ const favoriteBlog = (blogs) => {
 	return favoriteBlogFormatted;
 }
 
+// 4.6
+// Reference by Maheer Ali: https://stackoverflow.com/questions/55038993/how-to-sum-value-in-javascript-array-object-form-specific-search-id
+const mostBlogs = (blogs) => {
+	const nbBlogsPerAuthor = blogs.reduce((prev, current) => {
+		const index = prev.findIndex(x => x.author === current.author);
+		if (index === -1) {
+			current.nbPosts = 1
+			prev.push(current)
+		} else {
+			prev[index].nbPosts += 1
+		}
+		return prev;
+	}, [])
+
+	const mostBlogsPerAuthor = nbBlogsPerAuthor.reduce((prev, current) => prev.nbPosts > current.nbPosts ? prev : current);
+	const mostBlogsPerAuthorFormatted = {
+		author: mostBlogsPerAuthor.author,
+		blogs: mostBlogsPerAuthor.nbPosts
+	}
+
+	return mostBlogsPerAuthorFormatted;
+}
+
+// 4.7
 // Reference by Maheer Ali: https://stackoverflow.com/questions/55038993/how-to-sum-value-in-javascript-array-object-form-specific-search-id
 const mostLikes = (blogs) => {
 	const likesPerAuthor = blogs.reduce((prev, current) => {
@@ -32,7 +59,7 @@ const mostLikes = (blogs) => {
 		author: mostLikesAuthor.author,
 		likes: mostLikesAuthor.likes
 	}
-	
+
 	return mostLikesAuthorFormatted;
 }
 
@@ -40,5 +67,6 @@ module.exports = {
 	dummy,
 	totalLikes,
 	favoriteBlog,
+	mostBlogs,
 	mostLikes
 }
